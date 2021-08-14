02bf466849019c4470c7d329501347f0298be009
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-08-13 22:49:02 -0600

    When added, conditionals are included

--- myproject/src/App.svelte
+++ myproject/src/App.svelte
@@ -1,16 +1,34 @@
 <script>
   let people = [
-    // { name: "yoshi", beltColour: "black", age: 25, id: 1 },
-    // { name: "mario", beltColour: "orange", age: 45, id: 2 },
-    // { name: "luigi", beltColour: "brown", age: 35, id: 3 },
+    { name: "yoshi", beltColour: "black", age: 25, id: 1 },
+    { name: "mario", beltColour: "orange", age: 45, id: 2 },
+    { name: "luigi", beltColour: "brown", age: 35, id: 3 },
   ];
+
+  const handleClick = (id) => {
+    people = people.filter((person) => person.id != id);
+  };
+
+  let num = 5;
 </script>
+
+{#if num > 20}
+  <p>Greater than 20</p>
+{:else if num > 5}
+  <p>Greater than 5</p>
+{:else}
+  <p>Not greater than 5</p>
+{/if}
 
 <main>
   {#each people as person (person.id)}
     <div>
       <h4>{person.name}</h4>
+      {#if person.beltColour === "black"}
+        <p><strong>MASTER NINJA</strong></p>
+      {/if}
       <p>{person.age} years old, {person.beltColour} belt.</p>
+      <button on:click={() => handleClick(person.id)}>delete</button>
     </div>
   {:else}
     <p>There are no people to show...</p>
