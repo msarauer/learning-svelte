9153264794151dbfe9e9c8de96198de6587acb92
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-08-13 22:34:22 -0600

    When added, an each loop is added to show data

--- myproject/src/App.svelte
+++ myproject/src/App.svelte
@@ -1,27 +1,17 @@
 <script>
-  let firstName = "Mitchel";
-  let lastName = "Sarauer";
-  let beltColor = "black";
-
-  $: fullName = `${firstName} ${lastName}`;
-  $: {
-    console.log(beltColor);
-    console.log(fullName);
-  }
-
-  const handleClick = () => {
-    beltColor = "orange";
-  };
-  // const handleInput = (e) => {
-  //   beltColor = e.target.value;
-  // };
+  let people = [
+    { name: "yoshi", beltColour: "black", age: 25, id: 1 },
+    { name: "mario", beltColour: "orange", age: 45, id: 2 },
+    { name: "luigi", beltColour: "brown", age: 35, id: 3 },
+  ];
 </script>
 
 <main>
-  <p>{fullName} - {beltColor} belt</p>
-  <input type="text" bind:value={firstName} />
-  <input type="text" bind:value={lastName} />
-  <input type="text" bind:value={beltColor} />
+  {#each people as person (person.id)}
+    <div>
+      <h4>{person.name}</h4>
+      <p>{person.age} years old, {person.beltColour} belt.</p>
+    </div>{/each}
 </main>
 
 <style>
@@ -30,13 +20,6 @@
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
   }
 
   @media (min-width: 640px) {
