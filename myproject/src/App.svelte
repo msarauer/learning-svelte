5f2b8dee7c7e06313d100cde21bac0194d5771bf
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-08-13 22:35:42 -0600

    When added, an else statement is added

--- myproject/src/App.svelte
+++ myproject/src/App.svelte
@@ -1,8 +1,8 @@
 <script>
   let people = [
-    { name: "yoshi", beltColour: "black", age: 25, id: 1 },
-    { name: "mario", beltColour: "orange", age: 45, id: 2 },
-    { name: "luigi", beltColour: "brown", age: 35, id: 3 },
+    // { name: "yoshi", beltColour: "black", age: 25, id: 1 },
+    // { name: "mario", beltColour: "orange", age: 45, id: 2 },
+    // { name: "luigi", beltColour: "brown", age: 35, id: 3 },
   ];
 </script>
 
@@ -11,7 +11,10 @@
     <div>
       <h4>{person.name}</h4>
       <p>{person.age} years old, {person.beltColour} belt.</p>
-    </div>{/each}
+    </div>
+  {:else}
+    <p>There are no people to show...</p>
+  {/each}
 </main>
 
 <style>
